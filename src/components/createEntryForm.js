5cc41ddf14d37695a255264c2896e70a7b54f00a
Bhavy Airi <airiragahv@gmail.com>
2023-09-28 12:53:02 +0530

    Add api for create with several other ui features

---
+++ src/components/createEntryForm.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const FormContainer = styled.div`
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const FormLabel = styled.label`
+  font-weight: bold;
+`;
+
+const FormInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const FormButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const CreateEntryForm = ({ closeModal }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    rollNo: "",
+    company: "",
+    package: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send a Get request to your API to create a new entry
+    fetch("https://placement-site.onrender.com/api/tnp/placement/create/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          closeModal();
+        } else {
+          console.error("Error creating entry:", response.statusText);
+        }
+      })
+      .catch((error) => {
+        console.error("Error creating entry:", error);
+      });
+  };
+
+  return (
+    <FormContainer>
+      <Form onSubmit={handleSubmit}>
+        <FormLabel htmlFor="name">Name:</FormLabel>
+        <FormInput
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <FormLabel htmlFor="rollNo">Roll No.:</FormLabel>
+        <FormInput
+          type="text"
+          id="rollNo"
+          name="rollNo"
+          value={formData.rollNo}
+          onChange={handleChange}
+          required
+        />
+
+        <FormLabel htmlFor="company">Company:</FormLabel>
+        <FormInput
+          type="text"
+          id="company"
+          name="company"
+          value={formData.company}
+          onChange={handleChange}
+          required
+        />
+
+        <FormLabel htmlFor="package">Package:</FormLabel>
+        <FormInput
+          type="text"
+          id="package"
+          name="package"
+          value={formData.package}
+          onChange={handleChange}
+          required
+        />
+
+        <FormButton type="submit">Create</FormButton>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default CreateEntryForm;
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Modal from "react-modal";
+import CreateEntryForm from "./createEntryForm";
+
+const CreateButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  cursor: pointer;
+`;
 
 const DashboardContainer = styled.div`
   max-width: 800px;
@@ -7,19 +20,10 @@ const DashboardContainer = styled.div`
   padding: 20px;
 `;
 
-const CreateButton = styled.div`
-  background-color: #007bff;
-  color: #fff;
-  text-decoration: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  margin-bottom: 20px;
-  display: inline-block;
-`;
-
 const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
+  margin-top: 20px;
 `;
 
 const TableHeader = styled.thead`
@@ -43,21 +47,51 @@ const TableCell = styled.td`
   text-align: left;
 `;
 
+const ModalContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const ModalContent = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+  width: 80%;
+  max-width: 400px;
+`;
+
+const ModalHeader = styled.h2`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
 const Dashboard = () => {
   const [placements, setPlacements] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
-    // fetch student placement data from the API
+    // Fetch student placement data from the API
     fetch("https://placement-site.onrender.com/api/tnp/placement/list_all/")
       .then((response) => response.json())
       .then((data) => setPlacements(data))
       .catch((error) => console.error("Error fetching placements:", error));
   }, []);
 
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <DashboardContainer>
-      <h1>Student Placement Details</h1>
-      <button to="/create">Create Entry</button>
+      <h1>Student Placement Dashboard</h1>
+      <CreateButton onClick={openModal}>Create Entry</CreateButton>
       <Table>
         <TableHeader>
           <tr>
@@ -78,8 +112,30 @@ const Dashboard = () => {
           ))}
         </tbody>
       </Table>
+
+      {/* Modal for Creating Entries */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Create Entry Modal"
+        style={{
+          content: {
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          },
+        }}
+      >
+        <ModalContainer>
+          <ModalContent>
+            <ModalHeader>Create Entry</ModalHeader>
+            <CreateEntryForm closeModal={closeModal} />
+          </ModalContent>
+        </ModalContainer>
+      </Modal>
     </DashboardContainer>
   );
 };
 
 export default Dashboard;
+
